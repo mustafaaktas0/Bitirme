421a002a4a3b1b5ab596c89f29ef384f15ae273a
Mustafa <mustafaaktas791@gmail.com>
2022-04-23 16:38:29 +0300

    Add project files.

---
+++ Entities/DTOs/CarDetailDtoTest.cs
@@ -0,0 +1,19 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+   public  class CarDetailDtoTest :IDto
+    {
+        public int CarId { get; set; }
+        public int ColorId { get; set; }
+        public int BrandId { get; set; }
+        public string ModelYear { get; set; }
+        public string BrandName { get; set; }
+        public string CarDescription { get; set; }
+        public string CarColor { get; set; }
+        public decimal CarDailyPrice { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController :ControllerBase
+    {
+  
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            
+            
+                return Ok("deneme");
+            
+        }
+    }
+}
